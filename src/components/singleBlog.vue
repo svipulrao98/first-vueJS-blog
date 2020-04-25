409a68d1ec3627a43102ba9d11298820d11ede56
vipul rao <31448917+svipulrao98@users.noreply.github.com>
2020-04-26 00:10:21 +0530

    Update singleBlog.vue

--- src/components/singleBlog.vue
+++ src/components/singleBlog.vue
@@ -22,7 +22,7 @@ export default {
         }
     },
     created() {
-        this.$http.get('https://timepass-7e081.firebaseio.com/posts/' + this.id + '.json').then(function(data){
+        this.$http.get(/*<-Your firebase link->*/ + this.id + '.json').then(function(data){
           return data.json();
         }).then(function(data){
           this.blog=data;
